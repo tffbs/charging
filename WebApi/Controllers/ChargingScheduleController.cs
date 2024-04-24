6d6f64117d5e54d123631bc2fd7889bc5df219d2
Tamas G <tamas.gczi@gmail.com>
2024-04-24 22:04:12 +0200

    initial commit

---
+++ WebApi/Controllers/ChargingScheduleController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Entities;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChargingScheduleController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<ChargingTimeSpan> GetCharingSchedule([FromBody]Request request)
+        {
+            // placing the logic here temporarily, will move it to a calculator
+
+            var schedule = new List<ChargingTimeSpan>();
+            var carData = request.CarData;
+            var userSettings = request.UserSettings;
+            var currentStartingTime = request.StartingTime;
+            var leavingTime = TimeSpan.Parse(userSettings.LeavingTime);
+            var leavingDateTime = request.StartingTime.Add(leavingTime);
+
+            // conversion to percentage, as user settings contains percentage, while car data has kWh
+            var currentChargePercentage = carData.CurrentyBatteryLevel / carData.BatteryCapacity;
+
+            // check if we need to direct charge
+            if (currentChargePercentage < userSettings.DirectChargingPercentage)
+            {
+                // we need the kWh amount to know how long we need to charge
+                var directAmount = userSettings.DirectChargingPercentage / 100 * carData.BatteryCapacity;
+
+                // calculating time needed based on missing charge and charge power -> kWh / h = kW
+                var requiredTimeToReachDirectAmount = (directAmount - carData.CurrentyBatteryLevel) / carData.ChargePower;
+                // converting hours in decimal to DateTime -> might be some loss of accuracy due to decimal to double casting 
+                var endTime = request.StartingTime.AddHours((double)requiredTimeToReachDirectAmount);
+
+                // adjust starting time, we'll only need this info, the charge amount is irrelevant because of the check earlier
+                currentStartingTime = endTime;
+
+                // adding the time span to the response
+                schedule.Add(new ChargingTimeSpan(request.StartingTime, endTime, true));
+            }
+
+            // creating possible timespans based on the tariffs
+            // this is so that we can handle changing days
+            var tariffs = request.UserSettings.Tariffs;
+            var possibleTimeSpans = new List<TariffTimeSpan>();
+
+            while (DateTime.Compare(currentStartingTime, leavingDateTime) < 0)
+            {
+                // converting to timespan, as 
+                var currentTime = TimeOnly.FromDateTime(currentStartingTime);
+
+                // this foreach could be eliminated by sorting tariffs and using 
+                foreach (var t in tariffs)
+                {
+                    var start = TimeOnly.Parse(t.StartTime);
+                    var end = TimeOnly.Parse(t.EndTime);
+
+                    // IsBetween will return false if start is 23:00 and end is 2:00
+                    if (currentTime.IsBetween(start, end))
+                    {
+                        var endDateTime = currentStartingTime.Add(end - currentTime);
+                        var tariffEndDate = DateTime.Compare(endDateTime, leavingDateTime) <= 0  ? endDateTime : leavingDateTime;
+                        possibleTimeSpans.Add(new TariffTimeSpan(currentStartingTime, tariffEndDate , t.EnergyPrice));
+
+                        currentStartingTime = tariffEndDate;
+                    }
+                }
+            }
+
+            return schedule;
+        }
+    }
+}
---
+++ WebApi/Entities/Request.cs
@@ -0,0 +1,65 @@
+﻿namespace WebApi.Entities
+{
+    public record Request
+    {
+        public DateTime StartingTime { get; set; }
+        public UserSettings UserSettings { get; } = new UserSettings();
+        public CarData CarData { get; } = new CarData();
+    }
+
+    // <>
+    public record UserSettings
+    {
+        /// <summary>
+        /// The desired battery percentage of the battery of the car at Leaving Time, Example: 90
+        /// </summary>
+        public int DesiredStateOfCharge { get; set; }
+
+        /// <summary>
+        /// This is the leaving time of the user, for simplicity we assume it is the same for all days of the week, Example: 07:00
+        /// </summary>
+        public string LeavingTime { get; set; }
+
+        /// <summary>
+        /// This is the minimum percentage of the battery we will always charge directly. Example: 20. If the car battery level would be let's say 15%, always first charge up to 20% before doing anything else
+        /// </summary>
+        public int DirectChargingPercentage { get; set; }
+
+        /// <summary>
+        /// Energy prices vary during the day, as shown below. For simplicity you can assume that the pricesare the same for every day of the week.
+        /// </summary>
+        public List<Tariff> Tariffs { get; set; } = new List<Tariff>();
+    }
+
+    public record Tariff
+    {
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public decimal EnergyPrice { get; set; }
+    }
+
+    public record CarData
+    {
+        /// <summary>
+        /// The power in kW (kilowatt) used when when charging. You can assume 100% efficiency and that it is constant over time. Example: 9.6
+        /// </summary>
+        public decimal ChargePower { get; set; }
+
+        /// <summary>
+        /// The amount of energy in kWh (kilowatt hour) that the battery can store. Example: 55
+        /// </summary>
+        public decimal BatteryCapacity { get; set; }
+
+        /// <summary>
+        /// The level of charge in kWh the car battery at any one time, Example: 12
+        /// </summary>
+        public decimal CurrentyBatteryLevel { get; set; }
+    }
+
+    public record ChargingTimeSpan(DateTime StartTime, DateTime EndTime, bool IsCharging);
+
+    public record TariffTimeSpan(DateTime StartTime, DateTime EndTime, decimal EnergyPrice)
+    {
+        public bool IsCharging { get; set; } = false;
+    }
+}
